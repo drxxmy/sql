97e921f4d26cf8ec80c9c843d9b8fe6a71167fee
dreamy <dreamy.develop@gmail.com>
2026-04-18 18:56:47 +0500

    feat: patch staruml package to activate it

--- flake.nix
+++ flake.nix
@@ -11,7 +11,37 @@
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ staruml ];
+        packages = with pkgs; [
+          (staruml.overrideAttrs {
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/bin
+              mv opt $out
+              mv usr/share $out
+              rm -rf $out/share/doc
+
+              substituteInPlace $out/share/applications/staruml.desktop \
+                --replace-fail "/opt/StarUML/staruml" "$out/bin/staruml"
+
+              mkdir -p $out/lib
+              ln -s ${lib.getLib stdenv.cc.cc}/lib/libstdc++.so.6 $out/lib/
+              ln -s ${lib.getLib systemd}/lib/libudev.so.1 $out/lib/libudev.so.0
+
+              # Activate StarUML
+              mkdir -p $out/opt/StarUML/resources
+              cp -f ${./app.asar} $out/opt/StarUML/resources/app.asar
+
+              patchelf --interpreter ${bintools.dynamicLinker} \
+                --add-needed libGL.so.1 \
+                $out/opt/StarUML/staruml
+
+              ln -s $out/opt/StarUML/staruml $out/bin/staruml
+
+              runHook postInstall
+            '';
+          })
+        ];
         shellHook = ''
           tmuxinator .
         '';
